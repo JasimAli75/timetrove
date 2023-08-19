ce0e08715aaaf8259203858ebfe5519842b0f366
Jasim Ali Rajput <115194580+JasimAli75@users.noreply.github.com>
2023-08-19 19:49:26 +0500

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Jasim Ali Rajput new projects
+      Jasim Ali Rajput new project
     </main>
   );
 }

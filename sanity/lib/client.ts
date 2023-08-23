a7ccf11e04b37ca17042b9bdb0fdd8da9de1dd75
Jasim Ali Rajput <115194580+JasimAli75@users.noreply.github.com>
2023-08-23 23:28:27 +0500

    sanity update

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,8 +1,10 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId: `${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}`, // Your project ID
-  dataset: `${process.env.NEXT_PUBLIC_SANITY_DATASET}`, // The name of the dataset in your Sanity account
-  useCdn: false, // `false` if you want to ensure fresh data
-  apiVersion: "v2023-08-20", // Use a specific API version (default: latest)
+  projectId: `${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}`,
+  dataset: `${process.env.NEXT_PUBLIC_SANITY_DATASET}`,
+  token: `${process.env.NEXT_SANITY_TOKEN}`,
+
+  apiVersion: "v2023-07-31",
+  useCdn: false,
 });

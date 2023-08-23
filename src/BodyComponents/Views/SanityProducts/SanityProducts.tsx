b301a9ff78be6f0b90f46b59ec0ffe2d3b743b1e
Jasim Ali Rajput <115194580+JasimAli75@users.noreply.github.com>
2023-08-24 00:40:33 +0500

    card css

--- src/BodyComponents/Views/SanityProducts/SanityProducts.tsx
+++ src/BodyComponents/Views/SanityProducts/SanityProducts.tsx
@@ -4,7 +4,7 @@ import ProductCard from "../ProductCard/ProductCard";
 
 const SanityProducts = ({ ProductData }: any) => {
   return (
-    <div>
+    <div className="flex gap-5">
       {ProductData.map((data: any, index: number) => (
         <ProductCard oneProductData={data} key={index} />
       ))}

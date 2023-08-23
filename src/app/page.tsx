24da22d31145aa03a70d88cc34e08bdf7296d405
Jasim Ali Rajput <115194580+JasimAli75@users.noreply.github.com>
2023-08-23 23:43:00 +0500

    sanity trying to fix error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,22 +6,11 @@ import PromoProduct from "@/BodyComponents/Views/PromoProducts";
 import SanityProducts from "@/BodyComponents/Views/SanityProducts/SanityProducts";
 import ShipDetails from "@/BodyComponents/Views/ShippingDetails/ShipDetails";
 
-async function FetchAllProductData() {
-  let res = await fetch(`${BASE_PATH_FOR_API}/api/product`);
-
-  if (!res.ok) {
-    throw new Error("failed to fetch data");
-  }
-  return res.json();
-}
-
-export default async function Home() {
-  let { response } = await FetchAllProductData();
-
+export default function Home() {
   return (
     <main>
       <Hero />
-      <SanityProducts ProductData={response} />
+      <SanityProducts />
       <ProductCategory />
       <ShipDetails />
       <PromoProduct />

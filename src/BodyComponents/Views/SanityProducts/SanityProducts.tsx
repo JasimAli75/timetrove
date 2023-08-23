b67205e5706c72191864ad8d3e2b05cf855acd09
Jasim Ali Rajput <115194580+JasimAli75@users.noreply.github.com>
2023-08-23 23:52:18 +0500

    sanity delete

--- src/BodyComponents/Views/SanityProducts/SanityProducts.tsx
+++ src/BodyComponents/Views/SanityProducts/SanityProducts.tsx
@@ -1,13 +1,20 @@
-import ProductCard from "../ProductCard/ProductCard";
+// import ProductCard from "../ProductCard/ProductCard";
 
-const SanityProducts = ({ ProductData }: any) => {
-  return (
-    <div className="flex gap-5">
-      {ProductData.map((data: any, index: number) => (
-        <ProductCard oneProductData={data} key={index} />
-      ))}
-    </div>
-  );
-};
+// const SanityProducts = ({ ProductData }: any) => {
+//   return (
+//     <div className="flex gap-5">
+//       {ProductData.map((data: any, index: number) => (
+//         <ProductCard oneProductData={data} key={index} />
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default SanityProducts;
+import React from "react";
+
+function SanityProducts() {
+  return <div>Jasim ali sanity</div>;
+}
 
 export default SanityProducts;
